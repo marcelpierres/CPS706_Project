2371bee165f0ce591ecf7446356cea9afe19c414
William Trieu <WilliamTrieu@users.noreply.github.com>
2016-12-02 20:48:57 -0500

    Add files via upload

---
+++ DNSRecord.java
@@ -0,0 +1,33 @@
+//DNS Record
+
+package CPS706;
+
+import java.io.*;
+import java.util.*;
+
+public class DNSRecord{
+	public String address, type, name;
+	
+	//set methods
+	public void setAddress(String a){
+		address = a;
+	}
+	public void setType(String t){
+		type = t;
+	}
+	public void setName(String n){
+		name = n;
+	}
+	
+	//get methods
+	public string getAddress{
+		return address;
+	}
+	public string getType{
+		return type;
+	}
+	public string getName{
+		return name;
+	}
+	
+}
